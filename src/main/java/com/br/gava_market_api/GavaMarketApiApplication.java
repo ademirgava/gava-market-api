5a94a765e7cde49aaf80204cb52c68760cb633d7
Juninho Gava <juninhog@gmail.com>
2025-03-26 11:07:30 -0300

    Initial commit

---
+++ src/main/java/com/br/gava_market_api/GavaMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.gava_market_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GavaMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GavaMarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/gava_market_api/GavaMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gava_market_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GavaMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
